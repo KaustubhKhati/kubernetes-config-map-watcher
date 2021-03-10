f4c960f22cc5fb6d35f707ca853d6031f51e5dc2
Kaustubh Khati <kaustubh.khati@tesco.com>
2021-03-10 13:12:05 +0530

    changed to watch config map changes

--- index.js
+++ index.js
@@ -1,39 +1,52 @@
-process.env.KUBECONFIG = './kube-config'
+process.env.KUBECONFIG = '<ABSOLUTE PATH TO KUBECONFIG>' //ONLY FOR LOCAL TESTING
 const k8s = require('@kubernetes/client-node');
 const kc = new k8s.KubeConfig();
 kc.loadFromDefault();
 const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
-const autoK8sApi = kc.makeApiClient(k8s.AutoscalingV2beta2Api)
-const cron = require('cron');	
 
+function watchConfigMap(){
+	const watch = new k8s.Watch(kc);
+	console.log('watching started');
+	const configMapURL = '/api/v1/namespaces/ecg-ppe/configmaps';
+	const options = {
+        allowWatchBookmarks: true,
+    }; 
+    const handleChange = (type, apiObj, watchObj) => {
+        if (type === 'MODIFIED') {
+            // tslint:disable-next-line:no-console
 
-async function checkForMemoryPressure(){
-	try{
-		const nodeList = await k8sApi.listNode()
-			.then(response => {
-				return response.body.items.map(v1Node => v1Node.metadata.name)
-			});
-		nodeList.forEach(node => {
-			k8sApi.readNodeStatus(node)
-				.then(response => {
-					const conditions = response.body.status.conditions.filter(condition => {	
-						
-						if(condition.type == 'MemoryPressure' && condition.status == 'True'){
-							return condition
-						}
-					})
-					if(conditions.length > 0){
-						console.log(response.body.metadata.name, conditions[0].status, new Date(), 'Pressure Found')
-					}
-			});
-		})
-		console.log(`Loop Passed at ${new Date()}`)
-	}
-	catch(error){
-		console.log(error)
-	}
+            console.log(watchObj.object.metadata.name)
+            if( watchObj.object.metadata.name == 'active-dc-config-map'){
+            	const data = JSON.parse(watchObj.object.data.config);
+            	console.log(data)
+            }
+        } else if (type === 'DELETED') {
+            // tslint:disable-next-line:no-console
+            console.log('deleted object:');
+        } else if (type === 'BOOKMARK') {
+            // tslint:disable-next-line:no-console
+            console.log(`bookmark: ${watchObj.metadata.resourceVersion}`);
+        }
+    }
+    const handleError = function err(err) {
+        // tslint:disable-next-line:no-console
+        if(!err){
+        	console.log('exited')
+        }
+        console.log(err);
+        startWatchAgain();
+    };
+	watch.watch(configMapURL, options, handleChange, handleError)
 }
 
-cron.job('* * * * * ', async () => {
-	checkForMemoryPressure()	
-}, null, true, null, this);	
+function startWatchAgain() {
+	watchConfigMap()
+}
+
+// app.listen('9000', () => {
+// 	console.log('Started')
+// 	watchConfigMap();
+// });
+
+watchConfigMap();
+
